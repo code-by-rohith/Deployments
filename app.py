d4e633156dbd3edefb6f8a35744b0422bc2e6e11
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-10-22 19:59:40 +0530

    first commit

--- app.py
+++ app.py
@@ -1,51 +1,55 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 import os
 
 app = Flask(__name__)
 
-# MongoDB connection
-mongo_uri = os.getenv("MONGODB_URI")
-client = MongoClient(mongo_uri)
-db = client['library']  # Replace with your database name
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client['library_db']  # Use your actual database name
 books_collection = db['books']
 
+# Home Route - List all books
 @app.route('/')
 def home():
-    return render_template('home.html')
-
-@app.route('/books')
-def book_list():
     books = books_collection.find()
     return render_template('book_list.html', books=books)
 
+# Add Book Route
 @app.route('/add', methods=['GET', 'POST'])
 def add_book():
     if request.method == 'POST':
         title = request.form['title']
         author = request.form['author']
         year = request.form['year']
-        books_collection.insert_one({'title': title, 'author': author, 'year': year})
+        books_collection.insert_one({
+            'title': title,
+            'author': author,
+            'year': year
+        })
         return redirect(url_for('home'))
     return render_template('add_book.html')
 
-@app.route('/issue', methods=['GET', 'POST'])
-def issue_book():
-    books = books_collection.find()
+# Edit Book Route
+@app.route('/edit/<book_id>', methods=['GET', 'POST'])
+def edit_book(book_id):
+    book = books_collection.find_one({'_id': ObjectId(book_id)})
     if request.method == 'POST':
-        book_id = request.form['book_id']
-        # Logic to issue the book goes here (you can store the issued status)
+        title = request.form['title']
+        author = request.form['author']
+        year = request.form['year']
+        books_collection.update_one(
+            {'_id': ObjectId(book_id)},
+            {"$set": {'title': title, 'author': author, 'year': year}}
+        )
         return redirect(url_for('home'))
-    return render_template('issue_book.html', books=books)
+    return render_template('edit_book.html', book=book)
 
-@app.route('/return', methods=['GET', 'POST'])
-def return_book():
-    books = books_collection.find()
-    if request.method == 'POST':
-        book_id = request.form['book_id']
-        # Logic to return the book goes here (you can store the returned status)
-        return redirect(url_for('home'))
-    return render_template('return_book.html', books=books)
+# Delete Book Route
+@app.route('/delete/<book_id>')
+def delete_book(book_id):
+    books_collection.delete_one({'_id': ObjectId(book_id)})
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
